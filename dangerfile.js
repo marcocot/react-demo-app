5bdafe377f99c9051280d894a9aeda143f5b9f57
Marco Cotrufo <marco.cotrufo@devncode.it>
2020-01-21 12:08:47 +0100

    github action flow

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,23 @@
-import { message, danger } from 'danger';
+import { message, danger, warn, schedule } from 'danger';
+import eslint from "@eaze/danger-plugin-eslint";
 
 const modified = danger.git.modified_files.join('- ');
 message('Changed files: \n- ' + modified);
+
+const changes = danger.git.modified_files.reduce((prev, filePath) => {
+  if (!prev.package) {
+    prev.package = filePath.includes('package.json');
+  }
+  if (!prev.lock) {
+    prev.lock = filePath.includes('package-lock.json');
+  }
+  return prev;
+}, {});
+
+if (changes.package && !changes.lock) {
+  const message = 'Changes were made to package.json, but not to package-lock.json';
+  const idea = 'Perhaps you need to run `npm install`?';
+  warn(`${message} - <i>${idea}</i>`);
+}
+
+schedule(eslint());
